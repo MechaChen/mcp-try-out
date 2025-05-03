db2339ca7c7af5d2090be134ff41d5705abf17a8
Benson Chen <tumlivein@gmail.com>
2025-05-04 01:11:12 +0800

    Add MCP Client - Query Processing Logic

--- mcp-client/index.ts
+++ mcp-client/index.ts
@@ -68,6 +68,63 @@ class MCPClient {
         }
     }
 
+    async processQuery(query: string) {
+        const messages: MessageParam[] = [
+            {
+                role: 'user',
+                content: query,
+            }
+        ]
+
+        const response = await this.anthropic.messages.create({
+            model: 'claude-3-5-sonnet-20240620',
+            max_tokens: 1000,
+            messages,
+            tools: this.tools,
+        })
+
+        const finalText = [];
+        const toolResults = [];
+
+        for (const content of response.content) {
+            if (content.type === 'text') {
+                finalText.push(content.text);
+            } else if (content.type === 'tool_use') {
+                const toolName = content.name;
+                const toolArgs = content.input as { [x: string]: unknown } | undefined;
+
+                const result = await this.mcp.callTool({
+                    name: toolName,
+                    arguments: toolArgs,
+                });
+
+                toolResults.push(result);
+                finalText.push(
+                    `[Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}]`
+                );
+
+                messages.push({
+                    role: 'user',
+                    content: result.content as string,
+                });
+
+                const response = await this.anthropic.messages.create({
+                    model: 'claude-3-5-sonnet-20240620',
+                    max_tokens: 1000,
+                    messages,
+                });
+
+                finalText.push(
+                    response.content[0]?.type === 'text'
+                        ? (response.content[0] as { text: string }).text
+                        : ''
+                );
+            }
+        }
+
+        return finalText.join('\n');
+    }
+
     async chatLoop() {
 
     }
